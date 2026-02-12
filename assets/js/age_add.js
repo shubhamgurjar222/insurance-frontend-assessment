f1f3bc6534e288131d7962892d3340e470e4ef67
Shubham Gurjar <shubhamgurjar222@gmail.com>
2026-02-12 11:34:31 +0530

    fix: update border-radius for autofill input for improved aesthetics

--- assets/js/age_add.js
+++ assets/js/age_add.js
@@ -2,6 +2,13 @@
 const ageContainer = document.getElementById("age-counter");
 const addBtn = document.getElementById("addAgeBtn");
 
+    const input = document.createElement("input");
+    input.type = "text";
+    input.className = "counter-btn-blank";
+    
+    ageContainer.insertBefore(input, addBtn);
+
+
 addBtn.addEventListener("click", () => {
     const input = document.createElement("input");
     input.type = "text";
